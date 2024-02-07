254f4ec4bb8c5a3aeeccdaa28129c3a99495dfc2
2357111317192329 <76840561+2357111317192329@users.noreply.github.com>
2024-02-07 10:32:09 +0800

    Update ball.js

--- new/ball.js
+++ new/ball.js
@@ -1,15 +1,15 @@
 class ball {
-  constructor(r,x,y) {
+  constructor(r=20,x,y,vx=0,vy=0,bb=0.9) {
     this.r = r;
     this.x= x;
     this.y= y;
-    this.vx=0;
-    this.vy=0;
-    this.bb=0.9;
+    this.vx=vx;
+    this.vy=vy;
+    this.bb=bb;
   }
 
   // 定義一個物件方法
   lookdata() {
-    console.log(`r:${this.r} x:${this.x} y:${this.y}`);
+    console.log(`r:${this.r} x:${this.x} y:${this.y} vx:${this.vx} vy:${this.vy} bb:${this.bb}`);
   }
 }
