46be024d835873e86a6d1be365ad78ddfcbdf589
2357111317192329 <76840561+2357111317192329@users.noreply.github.com>
2024-03-15 07:00:16 +0800

    Create keyboard.js

---
+++ new/keyboard.js
@@ -0,0 +1,30 @@
+class KeyTracker {
+  constructor() {
+    this.keys = {
+      ArrowUp: false,
+      ArrowDown: false,
+      ArrowLeft: false,
+      ArrowRight: false,
+    };
+    document.addEventListener('keydown', this.onKeyDown.bind(this));
+    document.addEventListener('keyup', this.onKeyUp.bind(this));
+  }
+  onKeyDown(event) {
+    const key = event.key;
+    if (this.keys.hasOwnProperty(key)) {
+      this.keys[key] = true;
+    }
+  }
+  onKeyUp(event) {
+    const key = event.key;
+    if (this.keys.hasOwnProperty(key)) {
+      this.keys[key] = false;
+    }
+  }
+  isKeyPressed(key) {
+    if (this.keys.hasOwnProperty(key)) {
+      return this.keys[key];
+    }
+    return false;
+  }
+}
