5bae7a622fd42137f159d88d76f53c48801db8b6
2357111317192329 <76840561+2357111317192329@users.noreply.github.com>
2024-03-15 07:38:00 +0800

    Update keyboard.js

--- new/keyboard.js
+++ new/keyboard.js
@@ -8,20 +8,22 @@ class KeyTracker {
     };
     document.addEventListener('keydown', this.onKeyDown.bind(this));
     document.addEventListener('keyup', this.onKeyUp.bind(this));
-    this.interval = null;
   }
+
   onKeyDown(event) {
     const key = event.key;
     if (this.keys.hasOwnProperty(key)) {
       this.keys[key] = true;
     }
   }
+
   onKeyUp(event) {
     const key = event.key;
     if (this.keys.hasOwnProperty(key)) {
       this.keys[key] = false;
     }
   }
+
   isKeyPressed(key) {
     if (this.keys.hasOwnProperty(key)) {
       return this.keys[key];
