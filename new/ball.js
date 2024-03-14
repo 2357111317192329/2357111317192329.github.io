fd4a96537ac50c94c4c7b814dc6a830be9bd7365
2357111317192329 <76840561+2357111317192329@users.noreply.github.com>
2024-03-15 06:53:33 +0800

    Update ball.js

--- new/ball.js
+++ new/ball.js
@@ -14,23 +14,4 @@ class ball {
   gravity(g=2) {
     this.vy=this.vy+g;
   }
-  /*control(stepx=10,stepy=10) {
-    //無效，下次新增鍵盤類別以儲存按鍵是否被按下
-    window.addEventListener('keypress', function(event) {
-      switch (event.key) {
-        case "ArrowUp":
-          this.vy-=stepy;
-          break;
-        case "ArrowDown":
-          this.vy+=stepy;
-          break;
-        case "ArrowLeft":
-          this.vx-=stepx;
-          break;
-        case "ArrowRight":
-          this.vx+=stepx;
-          break;
-      }
-    }
-  }*/
 }
