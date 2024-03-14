a2fea8ca2d11f64a540716e771089edb95ee278a
2357111317192329 <76840561+2357111317192329@users.noreply.github.com>
2024-03-15 07:57:57 +0800

    Update script.js

--- new/script.js
+++ new/script.js
@@ -3,7 +3,7 @@ let ball0 = new ball(100,100,0,50);
 alert("Hello, World!");
 const keyTracker = new KeyTracker();
 setInterval(() => {
-  if (keyTracker.isKeyPressed('ArrowUp')) {
+  if (keyTracker.onKeyDown('ArrowUp')) {
     alert('YES');
   }
 }, 2000);
