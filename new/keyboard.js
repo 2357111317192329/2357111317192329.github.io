057813f817cd14ac3a87c28508187282f4fb77eb
2357111317192329 <76840561+2357111317192329@users.noreply.github.com>
2024-03-15 08:11:32 +0800

    Update keyboard.js

--- new/keyboard.js
+++ new/keyboard.js
@@ -6,6 +6,7 @@ class KeyTracker {
       ArrowLeft: false,
       ArrowRight: false,
     };
+    this.keyTimers = {};
     document.addEventListener('keydown', this.onKeyDown.bind(this));
     document.addEventListener('keyup', this.onKeyUp.bind(this));
   }
@@ -14,6 +15,17 @@ class KeyTracker {
     const key = event.key;
     if (this.keys.hasOwnProperty(key)) {
       this.keys[key] = true;
+      if (!this.keyTimers[key]) {
+        this.keyTimers[key] = setInterval(() => {
+          if (this.keys[key]) {
+            // Key is still pressed
+            console.log(key + ' is pressed');
+          } else {
+            clearInterval(this.keyTimers[key]);
+            this.keyTimers[key] = null;
+          }
+        }, 100); // Check every 100 milliseconds
+      }
     }
   }
 
@@ -21,6 +33,10 @@ class KeyTracker {
     const key = event.key;
     if (this.keys.hasOwnProperty(key)) {
       this.keys[key] = false;
+      if (this.keyTimers[key]) {
+        clearInterval(this.keyTimers[key]);
+        this.keyTimers[key] = null;
+      }
     }
   }
 
