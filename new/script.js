d5331f17ed140576d11e2e8511a8031269064c96
2357111317192329 <76840561+2357111317192329@users.noreply.github.com>
2024-04-16 21:53:11 +0800

    Update script.js

--- new/script.js
+++ new/script.js
@@ -1,9 +1,19 @@
 const keyTracker = new KeyTracker();
+//Up is ArrowUp
+//0 is 0
+//A is A
+//a is a
 setInterval(() => {
-  if (keyTracker.isKeyPressed('KeyA')) {
+  if (keyTracker.isKeyPressed('a') || keyTracker.isKeyPressed('A')) {
     console.log('A is pressed');
   }
   else{
     console.log('A is not pressed');
+  }
+  if (keyTracker.isKeyPressed('ArrowUp')) {
+    console.log('up is pressed');
+  }
+  else{
+    console.log('up is not pressed');
   }
 }, 100);
