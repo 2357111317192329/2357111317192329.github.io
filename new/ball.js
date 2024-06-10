db8fcad04432f5bd90708edead4f3f8d16725eac
2357111317192329 <76840561+2357111317192329@users.noreply.github.com>
2024-06-11 05:54:05 +0800

    Update ball.js

--- new/ball.js
+++ new/ball.js
@@ -1,4 +1,5 @@
 class ball {
+  controlkey=['ArrowLeft', 'ArrowRight', 'ArrowUp','ArrowDown'];
   constructor(x,y,id,r=20,vx=0,vy=0,bb=0.9) {
     this.r = r;
     this.x= x;
@@ -13,6 +14,15 @@ class ball {
     console.log(`r:${this.r} x:${this.x} y:${this.y} vx:${this.vx} vy:${this.vy} bb:${this.bb}`);
   }
   gravity(g=2) {
-    this.vy=this.vy+g;
+    this.vy=this.vy-g;
+  }
+  move(delvx,delvy) {
+    this.vx=this.vx+delvx;
+    this.vy=this.vy+delvy;
+  }
+  update() {
+    this.gravity();
+    this.x=this.vx+this.x;
+    this.y=this.vy+this.y;
   }
 }
