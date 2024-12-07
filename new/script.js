53eeaf8e1e60ae1a8e943309d81e9040d9b99131
2357111317192329 <76840561+2357111317192329@users.noreply.github.com>
2024-12-08 03:33:43 +0800

    Update script.js

--- new/script.js
+++ new/script.js
@@ -7,6 +7,9 @@ const ball1 = new ball(100,0,1);
 setInterval(() => {
   let tmpleft=ball1.controlkey[0];
   let tmpright=ball1.controlkey[1];
+  let tmpup=ball1.controlkey[2];
+  let tmpdown=ball1.controlkey[3];
+  let tmpgd=ball1.controlkey[4]; // 增重鍵
   if (keyTracker.isKeyPressed(tmpleft)) {
     ball1.move(-1,0);
   }
@@ -16,6 +19,22 @@ setInterval(() => {
     ball1.move(1,0);
   }
   else{
+  }
+  if (keyTracker.isKeyPressed(tmpup)) {
+    ball1.move(0,1);
+  }
+  else{
+  }
+  if (keyTracker.isKeyPressed(tmpdown)) {
+    ball1.move(0,-1);
+  }
+  else{
+  }
+  if (keyTracker.isKeyPressed(tmpgd)) {
+    ball1.gain_dense(1);
+  }
+  else{
+    ball1.gain_dense(0);
   }
   ball1.update();
   ball1.lookdata();
