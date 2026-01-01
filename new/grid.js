9c700fd7000d46f78bbad3286bab8711b840afeb
2357111317192329 <76840561+2357111317192329@users.noreply.github.com>
2026-01-01 12:35:59 +0800

    Update grid.js

--- new/grid.js
+++ new/grid.js
@@ -1,1 +1,22 @@
-
+class grid {
+  constructor(x,y,id,l,h,bb=1.0) {
+    this.x= x;
+    this.y= y;
+    this.l= l;
+    this.h= h;
+    this.id= id;
+    this.bb=bb;
+  }
+  // 定義一個物件方法
+  lookdata() {
+    console.log(`x:${this.x} y:${this.y} l:${this.l} h:${this.h}`);
+  }
+  max(a,b) {
+    if(a>b){
+      return a;
+    }
+    else{
+      return b;
+    }
+  }
+}
